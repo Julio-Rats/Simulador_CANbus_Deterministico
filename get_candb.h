edfaffe764fe51249fd510846062a3eaec758838
Rats <juliocesarmmc@hotmail.com>
2019-04-29 03:39:19 -0300

    carrega frames via arquivo

---
+++ get_candb.h
@@ -0,0 +1,13 @@
+#ifndef CANDB
+#define  CANDB
+#include "fifo.h"
+#include <math.h>
+
+#define  BITS_FRAMES   48  // BITS
+#define  PAYLOAD_FRAME 64  // BITS
+#define  SPEED_BIT   (double)(pow(2,20)/1000)
+
+void   input_file(u_int8_t* path);
+double get_duration_frame(u_int8_t payload);
+
+#endif
